4b0396f7ebb81ac5a8a02daef66b351d7f8be3d0
Washington <Washington@WASHINGTON-PC>
2019-09-15 11:50:10 -0300

    adding navbar

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [CategoryListComponent, CategoryFormComponent],
   imports: [
     CommonModule,
-    CategoriesRoutingModule
+    CategoriesRoutingModule,
+    RouterModule
   ]
 })
 
