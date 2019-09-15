cccb8895ca2b722fab33bf64cb0ba02a7dda500b
Washington <Washington@WASHINGTON-PC>
2019-09-15 13:42:54 -0300

    adding intercepting http requests with angular-in-memory-web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//COMUNICAÇÃO COM OS DADOS FAKERS (in memory web api) DEVE SER REMOVIDO QUANDO PUBLICAR
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";//DEVE COMENTAR QUANDO PUBLICAR
+import { InMemoryDatabase } from "./in-memory-database";//DEVE COMENTAR QUANDO PUBLICAR
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)//DEVE COMENTAR QUANDO PUBLICAR
   ],
   providers: [],
   bootstrap: [AppComponent]
